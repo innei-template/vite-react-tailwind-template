da7b8e464dbe0b1437814c9ec07b5b9781c4b8f7
Innei <i@innei.in>
2024-07-13 12:48:50 +0800

    cleanup
    Signed-off-by: Innei <i@innei.in>

--- src/providers/stable-router-provider.tsx
+++ src/providers/stable-router-provider.tsx
@@ -1,18 +1,17 @@
 import { useLayoutEffect } from 'react'
-import type { NavigateFunction } from 'react-router-dom'
 import {
   useLocation,
   useNavigate,
   useParams,
   useSearchParams,
 } from 'react-router-dom'
+import type { NavigateFunction } from 'react-router-dom'
 
 import { setNavigate, setRoute } from '~/atoms/route'
 
 declare global {
   export const router: {
     navigate: NavigateFunction
-    showSettings: (initialTab?: string | undefined) => () => void
   }
   interface Window {
     router: typeof router
@@ -20,8 +19,6 @@ declare global {
 }
 window.router = {
   navigate() {},
-  // eslint-disable-next-line unicorn/consistent-function-scoping
-  showSettings: () => () => {},
 }
 
 /**
